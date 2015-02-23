e9fe9d47f0e913b855c56da88f9c60d07788bfd9
Johan Oelrich <johan.oelrich@live.se>
2015-02-23 11:21:53 +0100

    Added some parallelisation
    The sets of experiments for different complex counts start
    simultaneously. Not a huge improvement, but some. Most time is still
    spent counting neighbours in the high-count and large size sets.

--- simulant/simulation.cpp
+++ simulant/simulation.cpp
@@ -5,9 +5,7 @@
 #include "statistics.h"
 #include "settings.h"
 #include "complex.h"
-
-
-
+#include <ppl.h>
 std::vector<std::pair<double, double>> simulant::simulation::impl::experiment(double test_area_radius, double blob_radius, size_t complex_count, size_t experiment_runs)
 {
 	std::vector<std::vector<size_t>> counts_per_run;
@@ -26,6 +24,7 @@ void simulant::simulation::impl::print_results(const settings & settings, size_t
 	std::stringstream ss;
 	ss << "data_for_complex_count_" << complex_count << ".dat";
 	std::ofstream writer{ ss.str() };
+
 	for (size_t jj = 0; jj < blob_radius_results.size(); ++jj)
 	{
 		writer << settings.blob_size_radii[jj];
@@ -43,15 +42,15 @@ void simulant::simulation::simulate()
 {
 	settings settings{};
 
-	for (size_t ii = 0; ii < 5; ++ii)
-	{
+	concurrency::parallel_for(size_t(0), settings.number_of_complexes.size(),
+		[&](size_t ii){
 		std::vector<std::vector<std::pair<double, double>>> blob_radius_results;
-		for (size_t jj = 0; jj < 3; ++jj)
+		for (size_t jj = 0; jj < settings.blob_size_radii.size(); ++jj)
 		{
 			auto blob_size_jj_complex_count_ii =
 				simulant::simulation::impl::experiment(settings.test_area_radius, settings.blob_size_radii[jj], settings.number_of_complexes[ii], settings.experiment_runs);
 			blob_radius_results.push_back(blob_size_jj_complex_count_ii);
 		}
 		simulant::simulation::impl::print_results(settings, settings.number_of_complexes[ii], blob_radius_results);
-	}
+	});
 }
