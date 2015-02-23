fbfb5ba660a4a33a22d38859caf348412507d85b
Johan Oelrich <johan.oelrich@live.se>
2015-02-23 11:23:46 +0100

    Default settings update
    Changed the default blob radii and complex counts to try.

--- simulant/settings.cpp
+++ simulant/settings.cpp
@@ -3,6 +3,17 @@
 
 simulant::simulation::settings::settings() : test_area_radius(3500.0), experiment_runs(1000)
 {
-	blob_size_radii = { 0.25, 2.5, 25.0 };
-	number_of_complexes = { 100, 200, 400, 800, 1600 };
+	blob_size_radii =
+		{
+			0.625 / 2.0,
+			1.25 / 2.0,
+			2.5 / 2.0,
+			5.0 / 2.0,
+			10.0 / 2.0,
+			20.0 / 2.0,
+			40.0 / 2.0,
+			80.0 / 2.0,
+			160.0 / 2.0
+		};
+	number_of_complexes = { 125, 250, 500, 1000, 2000, 4000, 8000 };
 }
